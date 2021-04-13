e69a224613617c1c7eb76efd271330c6385fdd03
Iuri Magnago <iurimp91@gmail.com>
2021-04-13 18:55:43 -0300

    Funcionalidade de perguntar qual nome e quantos ingressos quer ao clicar no botão de compra + tratado o caso dos ingressos terem acabado

--- js/script.js
+++ js/script.js
@@ -12,11 +12,33 @@ function renderizarFilmes(filmes) {
         <div class="movie">
             <img src="${arrayFilmes[i].imagem}">
             <div class="title">${arrayFilmes[i].titulo}</div>
-            <button>
+            <button onclick="comprar(${arrayFilmes[i].id})">
             Comprar
             <ion-icon name="cart-outline"></ion-icon>
             </button>
         </div>
         `;
     }
+}
+
+let nome;
+let assentos;
+
+function comprar(idFilme) {
+    nome = prompt("Qual seu nome?");
+    assentos = prompt("Quantos ingressos você deseja?");
+
+    const dados = {nome: nome, quantidade: assentos};
+
+    const promessa = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${idFilme}/ingresso`, dados);
+    promessa.catch(tratarErro);
+    promessa.then(tratarAcerto);
+}
+
+function tratarErro() {
+    alert("Os ingressos para este filme estão esgotados!");
+}
+
+function tratarAcerto() {
+    alert(`${nome} comprou ${assentos} ingressos para o filme!`);
 }
